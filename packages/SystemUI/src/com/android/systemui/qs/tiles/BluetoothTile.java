5c84ff04862fc918a22e591bde7e54ab10c70eb7
AmeChanRain <downloadbot007@gmail.com>
2023-10-06 12:19:05 +0000

    Revert "SystemUI: Implement Bluetooth dialog"
    This reverts commit e38de813f10ebc46a9815210bcb0555c4cde071a.

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
@@ -50,7 +50,6 @@ import com.android.systemui.qs.QSHost;
 import com.android.systemui.qs.QsEventLogger;
 import com.android.systemui.qs.logging.QSLogger;
 import com.android.systemui.qs.tileimpl.QSTileImpl;
-import com.android.systemui.qs.tiles.dialog.BluetoothDialogFactory;
 import com.android.systemui.statusbar.policy.BluetoothController;
 
 import java.util.List;
@@ -67,9 +66,7 @@ public class BluetoothTile extends QSTileImpl<BooleanState> {
 
     private static final String TAG = BluetoothTile.class.getSimpleName();
 
-    private final Handler mHandler;
     private final BluetoothController mController;
-    private final BluetoothDialogFactory mBluetoothDialogFactory;
 
     private CachedBluetoothDevice mMetadataRegisteredDevice = null;
 
@@ -86,28 +83,27 @@ public class BluetoothTile extends QSTileImpl<BooleanState> {
             StatusBarStateController statusBarStateController,
             ActivityStarter activityStarter,
             QSLogger qsLogger,
-            BluetoothController bluetoothController,
-            BluetoothDialogFactory bluetoothDialogFactory
+            BluetoothController bluetoothController
     ) {
         super(host, uiEventLogger, backgroundLooper, mainHandler, falsingManager, metricsLogger,
                 statusBarStateController, activityStarter, qsLogger);
-        mHandler = mainHandler;
         mController = bluetoothController;
-        mBluetoothDialogFactory = bluetoothDialogFactory;
         mController.observe(getLifecycle(), mCallback);
         mExecutor = new HandlerExecutor(mainHandler);
     }
 
     @Override
     public BooleanState newTileState() {
-        BooleanState s = new BooleanState();
-        s.forceExpandIcon = true;
-        return s;
+        return new BooleanState();
     }
 
     @Override
     protected void handleClick(@Nullable View view) {
-        mHandler.post(() -> mBluetoothDialogFactory.create(true, view));
+        // Secondary clicks are header clicks, just toggle.
+        final boolean isEnabled = mState.value;
+        // Immediately enter transient enabling state when turning bluetooth on.
+        refreshState(isEnabled ? null : ARG_SHOW_TRANSIENT_ENABLING);
+        mController.setBluetoothEnabled(!isEnabled);
     }
 
     @Override
